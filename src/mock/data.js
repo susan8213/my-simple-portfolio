aea48a2fbb7b12bff108aff9a0a5ed7588f35dcf
Ying Hsiu Lai <susan8213@gmail.com>
2024-04-07 22:18:03 +0800

    [content] Modify major data

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,52 +2,62 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Joellyn | Web Developer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: "Joellyn's Portfolio", // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'Hi, my name is',
+  name: 'Joellyn',
+  subtitle: "I'm a Web Developer",
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    'Backend Developer / Machine Learning Engineer with 3+ years experience at a global tech company, responsible for several web service projects which aim for reducing human cost and enhancing customers/partner relationships.',
+  paragraphTwo: ' ',
+  paragraphThree: ' ',
+  resume: 'https://joellyn.notion.site/Resume-Joellyn-8bc3079834dd44b6895af0995fb4610e', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
+    img: 'my-awesome-portfolio.png',
+    title: 'Personal Blog',
     info: '',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://susan8213.netlify.app',
+    repo: 'https://github.com/susan8213/my-awesome-portfolio', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
+    img: 'smart-brain.png',
+    title: 'Smart Brain Web App',
     info: '',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://smartbrain-h9i3.onrender.com/',
+    repo: 'https://github.com/susan8213/smartbrain', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
+    img: 'robofriends.png',
+    title: 'Robot Friends Web App',
+    info: '',
+    info2: '',
+    url: 'https://susan8213.github.io/robofriends/',
+    repo: 'https://github.com/susan8213/robofriends', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'web-crawling.png',
+    title: 'Web Crawling',
     info: '',
     info2: '',
     url: '',
@@ -58,8 +68,8 @@ export const projectsData = [
 // CONTACT DATA
 export const contactData = {
   cta: '',
-  btn: '',
-  email: '',
+  btn: "Let's Connect!",
+  email: 'susan8213@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,28 +77,28 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'linkedin',
-      url: '',
+      name: 'envelope',
+      url: 'mailto:susasn8213@gmail.com',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/susan8213',
+    },
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/ying-hsiu-lai',
+    },
+    {
+      id: nanoid(),
+      name: 'file-text',
+      url: 'https://joellyn.notion.site/Resume-Joellyn-8bc3079834dd44b6895af0995fb4610e',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
